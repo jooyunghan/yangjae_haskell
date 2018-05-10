e776330aa48b05ec5bccdf0cd8f290261c8eebbf
Jooyung Han <jooyung.han@gmail.com>
2018-05-10 15:11:51 +0900

    Add String ch

--- Lambda/src/Lib.hs
+++ Lambda/src/Lib.hs
@@ -1,6 +1,13 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+sayHello name = putStrLn ("Hello, " ++ name)
+
+
+f x y = let z = x + y
+            zz = z * x
+        in let z = z
+               y = x
+           in
---
+++ String/ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for String
+
+## Unreleased changes
